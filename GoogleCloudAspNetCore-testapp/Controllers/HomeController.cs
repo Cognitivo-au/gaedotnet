6688b3677118f6de6381945914047923846f8a9c
vinaykumar <vinaykumar@VINAYSURFACE>
2017-07-17 20:02:21 +1000

    added team page

--- GoogleCloudAspNetCore-testapp/Controllers/HomeController.cs
+++ GoogleCloudAspNetCore-testapp/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GoogleCloudAspNetCore.testapp.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+
 
 namespace GoogleCloudAspNetCore_testapp.Controllers
 {
@@ -20,15 +25,71 @@ namespace GoogleCloudAspNetCore_testapp.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Skills()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your skills description page.";
 
             return View();
         }
 
+        public IActionResult team()
+        {
+            ViewData["Message"] = "Your team description page.";
+
+            return View();
+        }
+    
+
         public IActionResult Error()
         {
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+   
+     
+        [HttpPost]
+        public ActionResult Contact(ContactModels c)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msg = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    MailAddress from = new MailAddress(c.Email.ToString());
+                    StringBuilder sb = new StringBuilder();
+                    msg.To.Add("youremail@email.com");
+                    msg.Subject = "Contact Us";
+                    msg.IsBodyHtml = false;
+
+                    client.Host = "smtp.gmail.com";
+                    client.Port = 587;
+                    client.EnableSsl = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Credentials = new System.Net.NetworkCredential("vinay.samineni@cognitivo.com.au", "ganesh00");
+                              
+                    sb.Append("First name: " + c.FirstName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Last name: " + c.LastName);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Email: " + c.Email);
+                    sb.Append(Environment.NewLine);
+                    sb.Append("Comments: " + c.Comment);
+                    msg.Body = sb.ToString();
+                    smtp.Send(msg);
+                    msg.Dispose();
+                    return View("Success");
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+            }
             return View();
         }
     }
---
+++ GoogleCloudAspNetCore-testapp/Models/ContactModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GoogleCloudAspNetCore.testapp.Models
+{
+   public class ContactModels
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+    }
+}
