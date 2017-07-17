d298da42abf9895dcb14818ec5d0622f093dea7d
alan-cognitivo <alan.hsiao@cognitivo.com.au>
2017-07-17 22:01:53 +1000

    changes to views

--- GoogleCloudAspNetCore-testapp/Controllers/HomeController.cs
+++ GoogleCloudAspNetCore-testapp/Controllers/HomeController.cs
@@ -50,46 +50,13 @@ namespace GoogleCloudAspNetCore_testapp.Controllers
             return View();
         }
 
-   
-     
-        [HttpPost]
-        public ActionResult Contact(ContactModels c)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    MailMessage msg = new MailMessage();
-                    SmtpClient smtp = new SmtpClient();
-                    MailAddress from = new MailAddress(c.Email.ToString());
-                    StringBuilder sb = new StringBuilder();
-                    msg.To.Add("youremail@email.com");
-                    msg.Subject = "Contact Us";
-                    msg.IsBodyHtml = false;
 
-                    client.Host = "smtp.gmail.com";
-                    client.Port = 587;
-                    client.EnableSsl = true;
-                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                    client.Credentials = new System.Net.NetworkCredential("vinay.samineni@cognitivo.com.au", "ganesh00");
-                              
-                    sb.Append("First name: " + c.FirstName);
-                    sb.Append(Environment.NewLine);
-                    sb.Append("Last name: " + c.LastName);
-                    sb.Append(Environment.NewLine);
-                    sb.Append("Email: " + c.Email);
-                    sb.Append(Environment.NewLine);
-                    sb.Append("Comments: " + c.Comment);
-                    msg.Body = sb.ToString();
-                    smtp.Send(msg);
-                    msg.Dispose();
-                    return View("Success");
-                }
-                catch (Exception)
-                {
-                    return View("Error");
-                }
-            }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Resources()
+        {
             return View();
         }
     }
